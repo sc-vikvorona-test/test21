f82ebbdc468706211db2513073f3ac3c57b59a35
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-05-18 17:45:35 +0200

    incr-test: add new function to app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 let count = 0;
 const output = document.getElementById('output');
 
-document.getElementById('btn').addEventListener('click', () => {
-  count++;
-  output.textContent = `Clicked ${count} time${count === 1 ? '' : 's'}`;
-});
-
-document.getElementById('reset').addEventListener('click', () => {
+function resetCounter() {
   count = 0;
   output.textContent = '';
-});
+}
+
+function incrementCounter() {
+  count++;
+  output.textContent = "Clicked " + count + " time" + (count === 1 ? '' : 's');
+}
+
+document.getElementById('btn').addEventListener('click', incrementCounter);
+document.getElementById('reset').addEventListener('click', resetCounter);
 
 document.getElementById('msg-btn').addEventListener('click', () => {
   const msg = document.getElementById('msg-input').value;

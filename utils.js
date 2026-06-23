9949f6e1eb4c3b82bd2c3fd10a861293882452b6
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-06-23 22:36:08 +0200

    add utility helpers

---
+++ utils.js
@@ -0,0 +1,9 @@
+// Formats a number with thousand separators
+function formatNumber(n) {
+  return n.toLocaleString();
+}
+
+// Returns a greeting for the given name
+function greet(name) {
+  return 'Hello, ' + name + '!';
+}

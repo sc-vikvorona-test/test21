f544dadb5be31dfa3d04ed70c16dad2477c06f68
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-07-21 16:34:22 +0200

    test: add demo file with sonar issues (PR B)

---
+++ issue-demo-b.js
@@ -0,0 +1,29 @@
+function computeTotalB(items) {
+  var total = 0;
+  for (var i = 0; i < items.length; i++) {
+    total = total + items[i].price;
+  }
+  return total;
+}
+
+function isMatchB(a, b) {
+  if (a == b) {
+    return true;
+  }
+  return false;
+}
+
+function safeParseB(raw) {
+  try {
+    return JSON.parse(raw);
+  } catch (e) {
+  }
+  return null;
+}
+
+function buildSummaryB(order) {
+  var unusedDiscount = order.discount;
+  return `Order total: ${computeTotalB(order.items)}`;
+}
+
+module.exports = { computeTotalB, isMatchB, safeParseB, buildSummaryB };
